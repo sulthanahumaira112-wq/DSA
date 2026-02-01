28acf3b5a5199fc5bd2f27e4a9231800dcf3c7bc
Humaira I <sulthanahumaira112@gmail.com>
2026-02-01 07:44:32 +0530

    Time: 0 ms (100.00%) | Memory: 72.5 MB (93.75%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>ans(n);
+        
+        int maxi=-1;
+        for(int i=n-1;i>=0;i--){
+             ans[i]=maxi;
+            maxi=max(maxi,arr[i]);
+            
+        }return ans;
+    }
+};
